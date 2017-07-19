08da403e88309082a7ebc257dd6674fde5f69430
Casey Primozic <me@ameo.link>
2017-07-19 10:23:42 -0400

    Added absolute cargo path
     * When run with `cron`, `cargo`'s path was failing to be detected.  After supplying the absolute path, this finally works.

--- st3-cursor-color/run.sh
+++ st3-cursor-color/run.sh
@@ -2,4 +2,4 @@
 
 # Sets the color of the cursor in my current Sublime Text 3 theme file to a different color depending on the date.
 
-cargo build && ./target/debug/st3-cursor-color "/Users/casey/Library/Application Support/Sublime Text 3/Packages/User/SublimeLinter/kiwi (SL).tmTheme"
+/Users/casey/.cargo/bin/cargo build && ./target/debug/st3-cursor-color "/Users/casey/Library/Application Support/Sublime Text 3/Packages/User/SublimeLinter/kiwi (SL).tmTheme"
