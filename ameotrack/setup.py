a8acdcef80d8c7911ed6d0ca0c6ea45b0addd0f0
Casey Primozic <me@ameo.link>
2018-10-27 18:25:50 -0500

    Add `setup.py`

---
+++ ameotrack/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="ameotrack",
+    version="0.1.0",
+    packages=["ameotrack"],
+    entry_points={"console_scripts": ["ameotrack=ameotrack.__main__:main"]},
+    install_requires=[
+        "toml~=0.10",
+        "requests~=2.20",
+        "toml~=0.10",
+        "termcolor~=1.1",
+        "pyperclip~=1.7",
+    ],
+    license="MIT",
+    url="https://github.com/Ameobea/misc_scripts/tree/master/ameotrack",
+    author="Casey Primozic (Ameo)",
+    author_email="me@ameo.link",
+)
