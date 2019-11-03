0ba067da4122a0cb1eb55dd432d5fef41ab0f459
Casey Primozic <me@ameo.link>
2019-11-03 14:31:44 -0800

    Fix example conf file format
     * Use `exports.foo` rather than `export const foo = ` since the latter isn't supported by Node apparently

--- osutrack_deduper/conf.example.js
+++ osutrack_deduper/conf.example.js
@@ -2,7 +2,7 @@
  * COPY TO `conf.js` TO USE THE TOOL
  */
 
-export const db_host = 'example.com';
-export const db_user = 'username';
-export const db_pass = 'password';
-export const db_database = 'dbname';
+exports.db_host = 'example.com';
+exports.db_user = 'username';
+exports.db_pass = 'password';
+exports.db_userdb_database = 'dbname';
